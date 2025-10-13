1bf9558711b419ef94844a87b34badeaef7772b8
khaledsawan <khaled963sawan@gmail.com>
2025-10-13 16:42:52 +0300

    Add configuration files for GraphiQL and Lighthouse
    - Created graphiql.php to configure GraphiQL UI routes and settings.
    - Created lighthouse.php to configure GraphQL server routes, caching, security, pagination, and subscriptions.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,8 @@ use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance;
 use Webkul\Core\Http\Middleware\SecureHeaders;
 use Webkul\Installer\Http\Middleware\CanInstall;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -35,6 +37,16 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->append(SecureHeaders::class);
         $middleware->append(CanInstall::class);
+
+        /**
+        * Remove session and cookie middleware from the 'web' middleware group.
+        */
+        $middleware->removeFromGroup('web', [StartSession::class, AddQueuedCookiesToResponse::class]);
+
+        /**
+        * Adding session and cookie middleware globally to apply across non-web routes (e.g. GraphQL)
+        */
+        $middleware->append([StartSession::class, AddQueuedCookiesToResponse::class]);
 
         /**
          * Add the overridden middleware at the end of the list.
